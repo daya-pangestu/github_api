11c9e6dde883c821c86387cf660470dcc3643f48
ad <da>
2021-12-23 20:47:33 +0700

    add hilt application class

---
+++ app/src/main/java/com/daya/trawlbens_test_github_api/TrawlbensTest.kt
@@ -0,0 +1,7 @@
+package com.daya.trawlbens_test_github_api
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrawlbensTest : Application()
--- app/src/main/java/com/daya/trawlbens_test_github_api/view/MainActivity.kt
+++ app/src/main/java/com/daya/trawlbens_test_github_api/view/MainActivity.kt
@@ -3,7 +3,9 @@ package com.daya.trawlbens_test_github_api.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.daya.trawlbens_test_github_api.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
