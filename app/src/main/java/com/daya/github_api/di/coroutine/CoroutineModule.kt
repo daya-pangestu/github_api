7b956f266e526cdfb5c94c410a66cf480b49a4c8
ad <da>
2021-12-31 16:24:09 +0700

    add CoroutineModule

---
+++ app/src/main/java/com/daya/github_api/di/coroutine/CoroutineModule.kt
@@ -0,0 +1,26 @@
+package com.daya.github_api.di.coroutine
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutineModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+}
