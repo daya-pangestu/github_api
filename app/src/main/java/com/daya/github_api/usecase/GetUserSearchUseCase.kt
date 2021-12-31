2cec84696c6ab56c6650272ca201d8d6af8b7c95
ad <da>
2021-12-31 16:29:30 +0700

    add GetUserSearchUseCase.kt

---
+++ app/src/main/java/com/daya/github_api/usecase/GetUserSearchUseCase.kt
@@ -0,0 +1,21 @@
+package com.daya.github_api.usecase
+
+import com.daya.github_api.data.GithubRepository
+import com.daya.github_api.data.User
+import com.daya.github_api.di.coroutine.IoDispatcher
+import com.daya.github_api.utils.UseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class GetUserSearchUseCase
+@Inject
+constructor(
+    @IoDispatcher coroutineDispatcher: CoroutineDispatcher,
+    private val repo : GithubRepository
+) : UseCase<String,List<User>>(coroutineDispatcher){
+
+    override suspend fun execute(parameters: String): List<User> {
+        val incompleteData = repo.searchUser(parameters) //only contain name,avatar
+        return  repo.completingData(incompleteData)
+    }
+}
